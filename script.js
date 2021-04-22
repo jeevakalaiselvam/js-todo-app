eb94f9aa49b90def42cfddb336c9468d02a5a10c
Jeeva Kalaiselvam <jeevak001@outlook.com>
2021-04-23 02:28:03 +0530

    Local storage working

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const todosUlEl = document.getElementById("todos");
 
 formEl.addEventListener("submit", (e) => {
   e.preventDefault();
+  addTodoItem();
+});
+
+function addTodoItem() {
   const todoText = inputEl.value;
 
   if (todoText) {
@@ -11,5 +15,34 @@ formEl.addEventListener("submit", (e) => {
     todoLiEl.innerHTML = todoText;
     todosUlEl.appendChild(todoLiEl);
     inputEl.value = "";
+
+    todoLiEl.addEventListener("click", () => {
+      todoLiEl.classList.toggle("completed");
+      storeLocalStorage();
+    });
+
+    todoLiEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault;
+      todoLiEl.remove();
+    });
+
+    storeLocalStorage();
   }
-});
+}
+
+function storeLocalStorage() {
+  const todosEl = document.querySelectorAll("li");
+
+  const todos = [];
+
+  todosEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerHTML,
+      completed: todoEl.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function loadLocalStorage() {}
