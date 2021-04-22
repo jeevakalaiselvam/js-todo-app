b62ff4e22a1cfdb69353ef03de2055bd3f0ca8cd
Jeeva Kalaiselvam <jeevak001@outlook.com>
2021-04-23 01:08:31 +0530

    Form append completed

---
+++ script.js
@@ -0,0 +1,14 @@
+const formEl = document.getElementById("form");
+const inputEl = document.getElementById("input");
+const todosUlEl = document.getElementById("todos");
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todoText = inputEl.value;
+
+  if (todoText) {
+    const todoLiEl = document.createElement("li");
+    todoLiEl.innerHTML = todoText;
+    todosUlEl.appendChild(todoLiEl);
+  }
+});
